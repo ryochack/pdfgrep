e186e3b98da56ec97ce2ee52aa7852283e8826d1
ryochack <ryochack@gmail.com>
2017-12-09 03:24:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# pdfgrep.sh
+grep script for pdf files.
+
+## Requirements
+pdfgrep scripts requires `pdftotext` command is included as part of the poppler-utils package.
+
+## How to use
+```
+$ pdfgrep.sh <pattern> [path]
+```
+
---
+++ pdfgrep.sh
@@ -0,0 +1,31 @@
+#!/bin/env sh
+
+# pdfgrep.sh <pattern> [directory or pdf path]
+
+set -u
+IFS=$'\n'
+
+if [ $# -lt 1 ]; then
+	exit
+fi
+
+pdflist="*.pdf"
+pattern=$1
+
+if [ $# -ge 2 ]; then
+	source_path=$2
+	if [ -f $source_path ]; then
+		pdflist=$source_path
+	elif [ -d $source_path ]; then
+		directory=$source_path
+		pdflist="${directory}/*.pdf"
+	else
+		exit
+	fi
+fi
+
+for file in `ls ${pdflist}`
+do
+	pdftotext -nopgbrk -raw $file - 2> /dev/null | grep -i "${pattern}" | sed "s|^|${file}: |"
+done
+
